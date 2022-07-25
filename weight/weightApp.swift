06d2ef1e8a7881d70c70155493afe934cfe05a43
berkay <berkayoruc@hotmail.com>
2022-07-25 12:28:45 +0300

    Initial Commit

---
+++ weight/weightApp.swift
@@ -0,0 +1,17 @@
+//
+//  weightApp.swift
+//  weight
+//
+//  Created by berkay on 25.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct weightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
